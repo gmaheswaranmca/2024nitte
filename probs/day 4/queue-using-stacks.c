352d0ed0e1088956702ad5d81f96f422819e2056
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-06-11 15:07:59 +0530

    queue using stacks required stack simulation

--- probs/day 4/queue-using-stacks.c
+++ probs/day 4/queue-using-stacks.c
@@ -1,31 +1,45 @@
 
 typedef struct {
-    
+    int top_index;
+    int items[1000];
 } MyStack;
 
-
+bool myStackEmpty(MyStack* obj);
 MyStack* myStackCreate() {
-    
+    MyStack* rstack = (MyStack*)malloc(sizeof(MyStack));
+    rstack->top_index = -1;
+    return rstack;
 }
 
 void myStackPush(MyStack* obj, int x) {
-    
+    obj->top_index++;
+    obj->items[obj->top_index] = x;
 }
 
 int myStackPop(MyStack* obj) {
-    
+    if(myStackEmpty(obj)){
+        return -1;
+    }
+    int peek = obj->items[obj->top_index];
+    obj->top_index--;
+    return peek;
 }
 
-int myStackTop(MyStack* obj) {
-    
+int myStackPeek(MyStack* obj) {
+    if(myStackEmpty(obj)){
+        return -1;
+    }
+    int peek = obj->items[obj->top_index];
+    return peek;
 }
 
 bool myStackEmpty(MyStack* obj) {
-    
+    return obj->top_index == -1;
 }
 
 void myStackFree(MyStack* obj) {
-    
+    free(obj);
+    obj = NULL;
 }
 
 typedef struct {
@@ -50,7 +64,7 @@ int myQueuePop(MyQueue* obj) {
     if(myStackEmpty(obj->rstack2)){
         while(!myStackEmpty(obj->rstack1)){
             myStackPush(obj->rstack2,
-                muStackPop(obj->rstack1));
+                myStackPop(obj->rstack1));
         }
     }
     //
@@ -66,7 +80,7 @@ int myQueuePeek(MyQueue* obj) {
     if(myStackEmpty(obj->rstack2)){
         while(!myStackEmpty(obj->rstack1)){
             myStackPush(obj->rstack2,
-                muStackPop(obj->rstack1));
+                myStackPop(obj->rstack1));
         }
     }
     //
@@ -88,7 +102,6 @@ void myQueueFree(MyQueue* obj) {
     free(obj);
     obj = NULL;
 }
-
 
 /**
  * Your MyQueue struct will be instantiated and called as such:
