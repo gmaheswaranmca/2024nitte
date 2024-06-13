a9b6b3186c347f480b5dbe891d5005cd7a91ff27
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-06-13 11:56:35 +0530

    strtok for each token

---
+++ probs/day 6/04-tokenize.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char name[255] = "Sannidhi is coming from Karkala, she takes college bus & reaches college.";
+    char separator[20] = " ,&.";//space, comma, and symbol, dot
+    char* word = strtok(name,separator);
+    while(word != NULL) { 
+        printf("%s\n",word); // each word
+        word = strtok(NULL,separator);//next next token/word
+    }
+    return 0;
+}
