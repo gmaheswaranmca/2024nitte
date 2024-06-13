3114b7ca08913f849dbe95047fed48b0967923ee
gmaheswaranmca <gmaheswaranmca@gmail.com>
2024-06-13 14:08:33 +0530

    multiple global scoped local accessible static variables

--- probs/day 6/06.fibb.c
+++ probs/day 6/06.fibb.c
@@ -9,9 +9,22 @@ int fibb_term() { // generator
     return next;
 }
 
+int series_term() { //5,6,8,11,15,....
+    static int first=5,diff=1;
+    int next = first;
+    first = first + diff;
+    diff++;
+    return next;
+}
+
 int main(){
+    printf("Fibb series:\n");
     for(int I=1; I <= 10; I++){
         printf("%d ",fibb_term());
+    }
+    printf("\n\nSeries 5,6,8,11,15,....:\n");
+    for(int I=1; I <= 10; I++){
+        printf("%d ",series_term());
     }
     return 0;
 }
